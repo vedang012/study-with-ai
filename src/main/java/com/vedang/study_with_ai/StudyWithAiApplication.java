b32f12a76b9ae466725607d9970fd91e678f309a
Vedang <techvedang17@gmail.com>
2026-01-12 22:55:09 +0530

    Initial Project Setup

---
+++ src/main/java/com/vedang/study_with_ai/StudyWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.vedang.study_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedang/study_with_ai/StudyWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedang.study_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
