10f78b31dcb6ccec8104de4e96f8c5e14e58ab2d
Vedang <techvedang17@gmail.com>
2026-01-18 13:23:25 +0530

    added feature : create quiz from notes using ai

--- src/main/java/com/vedang/studywithai/controller/AiController.java
+++ src/main/java/com/vedang/studywithai/controller/AiController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/ai/")
+@RestController
 public class AiController {
 
     @Autowired
@@ -15,6 +15,14 @@ public class AiController {
     @GetMapping("/flashcards")
     public ResponseEntity<?> generateFlashcards(String notes) {
         String response = aiService.createFlashcards(notes);
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/json")
+                .body(response);
+    }
+
+    @GetMapping("/quiz")
+    public ResponseEntity<?> generateQuiz(String notes) {
+        String response = aiService.createQuiz(notes);
         return ResponseEntity.ok()
                 .header("Content-Type", "application/json")
                 .body(response);
--- src/main/java/com/vedang/studywithai/service/AiService.java
+++ src/main/java/com/vedang/studywithai/service/AiService.java
@@ -30,4 +30,18 @@ public class AiService {
         return getAiResponseAsJson(flashcardPrompt);
     }
 
+    public String createQuiz(String notes) {
+        String quizPrompt =
+                "Convert the given notes into quiz questions"
+                + "Return only a valid JSON array, no markdown, no explanation, no extra text. "
+                + "Strictly follow these Rules : total 4 options, 3 incorrect options and 1 correct option, multiple correct options are not allowed"
+                + "Each quiz question must have exactly 6 fields - \"question\", \"a\", \"b\", \"c\", \"d\", and the \"answer\""
+                + "question is the question, a, b, c, d are the options and answer is the correction option"
+                + "Create all questions strictly based on the provided notes. \n\n"
+                + "Notes: \n"
+                + notes;
+
+        return getAiResponseAsJson(quizPrompt);
+    }
+
 }
